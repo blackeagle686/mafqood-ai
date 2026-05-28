8411acd7d998ddb7536f3b514b78ea35d5db4822
blackeagle686 <mathematecs1@gmail.com>
2026-05-29 00:09:37 +0300

    refactor: update serializers.py

--- ai_system/app/people/serializers.py
+++ ai_system/app/people/serializers.py
@@ -4,4 +4,8 @@ class PersonReportSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     last_seen = serializers.CharField(max_length=255, required=False, allow_blank=True)
     details = serializers.CharField(required=False, allow_blank=True)
+    age = serializers.IntegerField(required=False, allow_null=True)
+    lat = serializers.FloatField(required=False, allow_null=True)
+    long = serializers.FloatField(required=False, allow_null=True)
+    dna_str_loci = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     file = serializers.ImageField() # Matches HTML name="file"
