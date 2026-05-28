f8942b7c1df980f7393c92fa1793907569b033fc
blackeagle686 <mathematecs1@gmail.com>
2026-05-28 10:36:43 +0300

    Added DNASearchApiView to urls.py

--- ai_system/app/search/urls.py
+++ ai_system/app/search/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FaceSearchView
+from .views import FaceSearchView, DNASearchApiView
 
 urlpatterns = [
     path('face/', FaceSearchView.as_view(), name='face_search'),
-    path('video/', FaceSearchView.as_view(), name='video_search'), # Reusing FaceSearch for now or add VideoSearch
+    path('video/', FaceSearchView.as_view(), name='video_search'),
+    path('dna/', DNASearchApiView.as_view(), name='dna_search_api'),
 ]
